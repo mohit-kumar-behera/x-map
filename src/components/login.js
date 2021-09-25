59a09b6b07660fcc17dcf9761f54dc1ecfe65762
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2021-09-25 07:03:33 +0530

    add fetch calls

--- src/components/login.js
+++ src/components/login.js
@@ -1,34 +1,55 @@
-import React from 'react'
-
-
-
+import React from 'react';
+import axios from 'axios';
 
 function Login() {
+  const loginUser = async ev => {
+    try {
+      ev.preventDefault();
+      const formDataArr = [...new FormData(ev.target)];
+      const formData = Object.fromEntries(formDataArr);
+      const raw = JSON.stringify(formData);
+
+      const options = {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: raw,
+        redirect: 'follow',
+      };
+
+      const responseData = await fetch(
+        'http://127.0.0.1:8000/api/auth/token/',
+        options
+      );
+      const response = await responseData.json();
+      if (!responseData.ok) throw Error('Email or Password Incorrect');
+
+      localStorage.setItem('token', response.access);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className="login">
-
+    <form className="login" onSubmit={loginUser}>
       <input
-        type='text'
-        placeholder='Email address'
-        name='text'
-        className='descrip'
-
+        type="email"
+        placeholder="Email address"
+        name="email"
+        className="descrip"
       />
       <input
-        type='text'
-        placeholder='Password'
-        name='text'
-        className='descrip'
-
+        type="password"
+        placeholder="Password"
+        name="password"
+        className="descrip"
       />
 
       <button className="submit-button">SUBMIT</button>
-      <div className="icons">
-        
-
-      </div>
+      <div className="icons"></div>
     </form>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- src/components/register.js
+++ src/components/register.js
@@ -1,34 +1,53 @@
-import React from 'react'
-
-
-
+import React from 'react';
 
 function Register() {
+  const registerUser = async ev => {
+    try {
+      ev.preventDefault();
+      const formDataArr = [...new FormData(ev.target)];
+      const formData = Object.fromEntries(formDataArr);
+      const raw = JSON.stringify(formData);
+
+      const options = {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: raw,
+        redirect: 'follow',
+      };
+
+      const responseData = await fetch(
+        'http://127.0.0.1:8000/api/auth/register/',
+        options
+      );
+      const response = await responseData.json();
+      if (!responseData.ok) throw Error('user with this mail already exists');
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className="register">
-
+    <form className="register" onSubmit={registerUser}>
       <input
-        type='text'
-        placeholder='Email address'
-        name='text'
-        className='descrip'
-
+        type="email"
+        placeholder="Email address"
+        name="email"
+        className="descrip"
       />
       <input
-        type='text'
-        placeholder='Password'
-        name='text'
-        className='descrip'
-
+        type="password"
+        placeholder="Password"
+        name="password"
+        className="descrip"
       />
 
       <button className="submit-button">SUBMIT</button>
-      <div className="icons">
-        
-
-      </div>
+      <div className="icons"></div>
     </form>
-  )
+  );
 }
 
-export default Register
+export default Register;
